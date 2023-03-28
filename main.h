8b852b4985b1fcd66a52cea0babc7158fdffdf79
konditi1 <konditi1@gmail.com>
2023-03-28 11:50:52 -0700

    fena push after corection

--- main.h
+++ main.h
@@ -3,8 +3,5 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
-char buffer[BUFFER_SIZE];
-char *str = va_arg(args, char *);
-char new_str[1024] = {0};
 int _printf(const char *format, ...);
 #endif /* MAIN_H */
---
+++ main_1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main- function of the program.
+ *
+ * Return: An integer value indicating the success or failure.
+ */
+int main(void)
+{
+_printf("Hello, %s! Today is %d/%d/%d.\n", "world", 3, 28, 2023);
+return (0);
+}
+
